cbf831154f008d82bccd6f05b9e0e9e9b87ba475
ranjit977 <ranjit977@gmail.com>
2021-07-26 17:14:30 +1000

    First commit

--- routes/api/post.js
+++ routes/api/post.js
@@ -1,9 +1,42 @@
 const express = require('express');
 const router = express.Router();
+const isSignedIn = require('../../middleware/auth');
+const { createPost, getPostById, updatePost, deletePost } = require('../../controllers/post');
+
+//Fire when postId parameter is matched
+//store the post details in req.post
+router.param('postId', getPostById);
 
 // @route GET api/post
 // @desc Test route
-// @access Public
-router.get('/', (req, res) => res.send('Post route'))
+// @access Private
+router.get('/', isSignedIn, (req, res) => {
+    console.log(req.user);
+    res.send('Success')
+})
+
+// @route post api/post/create
+// @desc Test route
+// @access Private
+router.post('/create', isSignedIn, createPost);
+
+// @route post api/post/update/:postId
+// @desc Test route
+// @access Private
+router.put(
+    "/update/:postId",
+    isSignedIn,
+    updatePost
+);
+
+// @route post api/post/delete/:postId
+// @desc Test route
+// @access Private
+// Only author can delete post
+router.delete(
+    "/delete/:postId",
+    isSignedIn,
+    deletePost
+);
 
 module.exports = router;
