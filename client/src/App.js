70cc59352ee245f6a5109b70a6f1913e5fc1d14c
Sergey Ustinov <sergey.ustinov@sensand.com>
2021-08-03 13:08:14 +1000

    Add routes for sign in and sign up

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage.component";
 import Feed from "./pages/Feed/Feed.component";
 import NotFound from "./pages/404NotFoundPage/NotFound.component";
+import SignIn from "./pages/SignIn/SignIn.component";
+import SignUp from "./pages/SignUp/SignUp.component";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/feed" component={Feed} />
+          <Route path="/login" component={SignIn} />
+          <Route path="/signup" component={SignUp} />
           <Route path="*" component={NotFound} />
         </Switch>
       </Router>
